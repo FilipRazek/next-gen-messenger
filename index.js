c488e394423e400b8f6f35813a95dce1c0b5c0f0
Filip Razek <filip@razek.net>
2024-03-08 14:02:07 +0100

    :sparkles: Add getSingleMessage route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { testHandler } from './testHandler.js';
 import { getCount, incrementCount } from './cptHandler.js';
+import { getSingleMessage } from './messageHandler.js';
 const app = express();
 
 // Pour s'assurer que l'on peut faire des appels AJAX au serveur
@@ -13,6 +14,7 @@ app.use(function(req, res, next) {
 app.get('/test/*', testHandler);
 app.get('/cpt/query', getCount);
 app.post('/cpt/inc', incrementCount);
+app.get('/msg/get/:id', getSingleMessage);
 
 const PORT = 8080;
 app.listen(PORT, () => {
---
+++ messageHandler.js
@@ -0,0 +1,21 @@
+const allMsgs = ["Hello World", "foobar", "CentraleSupelec Forever"]
+
+function isPositiveInteger(value) {
+    return value.match(/^[0-9]+$/);
+}
+
+export function getSingleMessage(req, res) {
+    const { id } = req.params;
+    if (isPositiveInteger(id)) {
+        const index = Number.parseInt(id, 10);
+        if (index < allMsgs.length) {
+            // On modifie légèrement le format de la réponse par
+            // rapport à l'énoncé pour respecter la convention
+            // code de retour = 0 si tout s'est bien passé
+            res.json({ msg: allMsgs[index], code: 0 });
+            return;
+        }
+    }
+    // Le code de retour est alors 1 en cas d'erreur
+    res.json({ code: 1 });
+}
