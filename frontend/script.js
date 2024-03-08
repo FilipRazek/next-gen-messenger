9f0b97853f0aa0a8cd0a48a6fe7c0038ea9ef5d1
Filip Razek <filip@razek.net>
2024-03-08 15:50:57 +0100

    :sparkles: Add new message posting

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,10 @@
 const BASE_URL = "https://next-gen-messenger.onrender.com";
 
-fetch(`${BASE_URL}/msg/getAll`)
-    .then(response => response.json())
-    .then(updateMessages);
+function fetchMessages() {
+    fetch(`${BASE_URL}/msg/getAll`)
+        .then(response => response.json())
+        .then(updateMessages);
+}
 
 function buildListItem(text) {
     const listItem = document.createElement("li");
@@ -15,3 +17,24 @@ function updateMessages(messages) {
     container.replaceChildren(...messages.map(buildListItem));
 }
 
+function sendMessage() {
+    const messageInput = document.getElementById("message-input");
+    const message = messageInput.value;
+    fetch(`${BASE_URL}/msg/post`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ msg: message })
+    })
+        .then(response => response.json())
+        .then(({code}) => {
+            if (code === 0) {
+                fetchMessages();
+                messageInput.value = "";
+            } else {
+                alert("Failed to send message");
+            }
+        });
+}
+fetchMessages();
