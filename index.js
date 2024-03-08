59d08830a0060e9761324cd81c1dbddabac0dab9
Filip Razek <filip@razek.net>
2024-03-08 11:10:14 +0100

    :recycle: Clean main file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express = require('express'); //import de la bibliothèque Express
-var app = express(); //instanciation d'une application Express
+const express = require('express');
+const app = express();
 
 // Pour s'assurer que l'on peut faire des appels AJAX au serveur
 app.use(function(req, res, next) {
@@ -8,16 +8,9 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Ici faut faire faire quelque chose à notre app...
-// On va mettre les "routes"  == les requêtes HTTP acceptéés par notre application.
-
-app.get("/", function(req, res) {
-  res.send("Hello")
-})
-
-
-
-app.listen(8080); //commence à accepter les requêtes
-console.log("App listening on port 8080...");
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}...`);
+});
 
 
